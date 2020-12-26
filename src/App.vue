506fda07742a952962e30573879d3558efd3bf35
kbc18a11 <wada121400@gmail.com>
2020-12-26 23:23:43 +0900

    スロットによるコンテンツ配信

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <BlogPost
+    v-bind:style="{ fontSize: postFontSize + 'em' }"
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :title="post.title"
+    @ositayatunani="ositayatu = $event"
+    v-model="searchText"
+  />
+  <p>{{ ositayatu }}</p>
+  <p>{{ searchText }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import BlogPost from './components/BlogPost.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    BlogPost,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'My journey with Vue' },
+        { id: 2, title: 'Blogging with Vue' },
+        { id: 3, title: 'Why Vue is so fun' },
+      ],
+      postFontSize: 1,
+      ositayatu: '',
+      searchText: '',
+    };
   },
 };
 </script>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="blog-post">
+    <h4>{{ title }}</h4>
+    <button @click="$emit('ositayatunani', id)">Enlarge text</button>
+  </div>
+  <input
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'BlogPost',
+  props: {
+    id: {
+      type: Number,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    searchText: {
+      type: String,
+      default: '',
+    },
+    modelValue: '',
+  },
+  emits: ['ositayatunani'],
+};
+</script>
+
+<style>
+</style>
